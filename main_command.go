9bea23f5ec645676c81820865bc4d4c30a8a22a9
ChuanleiGuo <chuanleiguo@gmail.com>
2018-08-12 14:06:11 +0800

    added cgroups

--- main_command.go
+++ main_command.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/chuanleiguo/Ducker/cgroups/subsystems"
 	"github.com/chuanleiguo/Ducker/container"
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
@@ -16,14 +17,34 @@ var runCommand = cli.Command{
 			Name:  "ti",
 			Usage: "enable tty",
 		},
+		cli.StringFlag{
+			Name:  "m",
+			Usage: "memory limit",
+		},
+		cli.StringFlag{
+			Name:  "cpushare",
+			Usage: "cpushare limit",
+		},
+		cli.StringFlag{
+			Name:  "cpuset",
+			Usage: "cpuset limit",
+		},
 	},
 	Action: func(context *cli.Context) error {
 		if len(context.Args()) < 1 {
 			return fmt.Errorf("Missing container command")
 		}
-		cmd := context.Args().Get(0)
+		var cmdArray []string
+		for _, arg := range context.Args() {
+			cmdArray = append(cmdArray, arg)
+		}
 		tty := context.Bool("ti")
-		Run(tty, cmd)
+		resConf := &subsystems.ResourceConfig{
+			MemoryLimit: context.String("m"),
+			CpuSet:      context.String("cpuset"),
+			CpuShare:    context.String("cpushare"),
+		}
+		Run(tty, cmdArray, resConf)
 		return nil
 	},
 }
@@ -33,8 +54,7 @@ var initCommand = cli.Command{
 	Usage: "Init container process run user's process in container. Do not call it outside",
 	Action: func(context *cli.Context) error {
 		log.Infof("init come on")
-		cmd := context.Args().Get(0)
-		err := container.RunContainerInitProcess(cmd, nil)
+		err := container.RunContainerInitProcess()
 		return err
 	},
 }
--- run.go
+++ run.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"github.com/chuanleiguo/Ducker/cgroups"
+	"github.com/chuanleiguo/Ducker/cgroups/subsystems"
 	"github.com/chuanleiguo/Ducker/container"
 	log "github.com/sirupsen/logrus"
 	"os"
+	"strings"
 )
 
-func Run(tty bool, command string) {
-	parent := container.NewParentProcess(tty, command)
+func Run(tty bool, cmdArray []string, res *subsystems.ResourceConfig) {
+	parent, writePipe := container.NewParentProcess(tty)
+	if parent == nil {
+		log.Errorf("New parent process error")
+		return
+	}
 	if err := parent.Start(); err != nil {
 		log.Error(err)
 	}
+
+	cgroupManager := cgroups.NewCgroupManager("duckor-cgroup")
+	defer cgroupManager.Destroy()
+	cgroupManager.Set(res)
+	cgroupManager.Apply(parent.Process.Pid)
+
+	sendInitCommand(cmdArray, writePipe)
 	parent.Wait()
-	os.Exit(-1)
+}
+
+func sendInitCommand(comArray []string, writePipe *os.File) {
+	command := strings.Join(comArray, " ")
+	log.Infof("command all is %s", command)
+	writePipe.WriteString(command)
+	writePipe.Close()
 }
